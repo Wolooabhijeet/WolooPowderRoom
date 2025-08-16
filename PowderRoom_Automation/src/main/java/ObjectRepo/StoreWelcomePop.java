d2a138366abb8f1a824b83f1bdc1d109912c750e
Woloo iOS <ioswoloo@gmail.com>
2025-08-16 12:09:16 +0530

    storepop

--- PowderRoom_Automation/src/main/java/ObjectRepo/StoreWelcomePop.java
+++ PowderRoom_Automation/src/main/java/ObjectRepo/StoreWelcomePop.java
@@ -1,5 +1,42 @@
 package ObjectRepo;
 
-public class StoreWelcomePop {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+public class StoreWelcomePop {
+	public AppiumDriver driver;
+
+	public StoreWelcomePop(AppiumDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+	@AndroidFindBy(uiAutomator = "new UiSelector().text(\"Store\")")
+	private WebElement storeButton;
+
+	public WebElement getStoreButton() {
+	    return storeButton;
+	}
+
+	@AndroidFindBy(id = "in.woloo.www:id/go_to_store")
+	private WebElement goToStoreButton;
+
+	public WebElement getGoToStoreButton() {
+	    return goToStoreButton;
+	}
+
+	@AndroidFindBy(id = "in.woloo.www:id/go_to_services")
+	private WebElement goToServicesButton;
+
+	public WebElement getGoToServicesButton() {
+	    return goToServicesButton;
+	}
+
+	
+
+	
+	
 }
